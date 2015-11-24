82271a8df91a6e52f235b72ac81e33ff5a82deac
mariomacedo <mario.macedo@ludustechnology.com>
2015-11-24 16:52:09 +0000

    interface updated and buildDisplay started

--- JADE_helloWorld/src/EnergyEfficiencySensorsModel.java
+++ JADE_helloWorld/src/EnergyEfficiencySensorsModel.java
@@ -5,13 +5,16 @@ import sajas.wrapper.ContainerController;
 
 import sajas.sim.repast3.Repast3Launcher;
 
+import uchicago.src.reflector.ListPropertyDescriptor;
 import uchicago.src.sim.analysis.OpenSequenceGraph;
 import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.gui.DisplaySurface;
+import uchicago.src.sim.gui.Object2DDisplay;
 import uchicago.src.sim.space.Object2DTorus;
 import uchicago.src.sim.util.Random;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class EnergyEfficiencySensorsModel extends Repast3Launcher {
 	private ContainerController mainContainer;
@@ -20,15 +23,36 @@ public class EnergyEfficiencySensorsModel extends Repast3Launcher {
 	private DisplaySurface dsurf;
 	private Object2DTorus space;
 	private OpenSequenceGraph plot;
+	private double energyLossPerTick;
+
+	private enum MyBoolean { Yes , No };
+	private MyBoolean allowGroupsFormation, nearAgents;
 
 	public EnergyEfficiencySensorsModel() {
 		this.numberOfAgents = 100;
 		this.spaceSize = 100;
+		this.allowGroupsFormation = MyBoolean.No;
+		this.nearAgents = MyBoolean.No;
+		this.energyLossPerTick = 0.5;
+	}
+
+	@Override
+	public void setup() {
+		super.setup();
+
+		//property descriptor
+		Vector<MyBoolean> mb = new Vector<MyBoolean>();
+		for(int i=0; i<MyBoolean.values().length; i++) {
+			mb.add(MyBoolean.values()[i]);
+		}
+
+		descriptors.put("AllowGroupsFormation", new ListPropertyDescriptor("MyBoolean", mb));
+		descriptors.put("NearAgents", new ListPropertyDescriptor("MyBoolean", mb));
 	}
 
 	@Override
 	public String[] getInitParam() {
-		return new String[] { "numberOfAgents" } ;
+		return new String[] { "numberOfAgents", "energyLossPerTick",  "allowGroupsFormation", "nearAgents" } ;
 	}
 
 	@Override
@@ -36,8 +60,21 @@ public class EnergyEfficiencySensorsModel extends Repast3Launcher {
 		return "Energy efficiency in a multi-agent system";
 	}
 
+	public void buildDisplay() {
+		//display and surface
+		Object2DDisplay display = new Object2DDisplay(space);
+		display.setObjectList(agentList);
+		dsurf.addDisplayableProbeable(display, "River");
+		dsurf.display();
+
+		//graph
+		if(plot != null) plot.dispose();
+		plot = new OpenSequenceGraph("Energy Evolution Curves", this);
+		plot.setAxisTitles("time", "energy");
+		//
+	}
+
 	protected void launchJADE() {
-		
 		Runtime rt = Runtime.instance();
 		Profile p1 = new ProfileImpl();
 		mainContainer = rt.createMainContainer(p1);
@@ -84,5 +121,37 @@ public class EnergyEfficiencySensorsModel extends Repast3Launcher {
 
 	public void setNumberOfAgents(int numberOfAgents) {
 		this.numberOfAgents = numberOfAgents;
+	}
+
+	public MyBoolean getNearAgents() {
+		return nearAgents;
+	}
+
+	public void setNearAgents(MyBoolean nearAgents) {
+		this.nearAgents = nearAgents;
+	}
+
+	public MyBoolean getAllowGroupsFormation() {
+		return allowGroupsFormation;
+	}
+
+	public void setAllowGroupsFormation(MyBoolean allowGroupsFormation) {
+		this.allowGroupsFormation = allowGroupsFormation;
+	}
+
+	public double getEnergyLossPerTick() {
+		return energyLossPerTick;
+	}
+
+	public void setEnergyLossPerTick(double energyPerTick) {
+		this.energyLossPerTick = energyPerTick;
+	}
+
+	public int getSpaceSize() {
+		return spaceSize;
+	}
+
+	public void setSpaceSize(int spaceSize) {
+		this.spaceSize = spaceSize;
 	}
 }
